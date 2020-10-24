60239e91d935bca5750aff2d31d10855b5bd499c
Mathew-Tomaney <mat.tomaney@gmail.com>
2020-10-24 17:22:06 +0100

    add component for spell list items

--- src/components/spellList.vue
+++ src/components/spellList.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="spell-list" v-if="spells.length">    
-        <li v-for="(spell) in this.spells" :key="spell.index"> {{spell.name}}  Level: {{spell.level}}  School: {{spell.school.index}}  Casting Time: {{spell.casting_time}}</li>
+        <spell-list-item v-for="(spell) in this.spells" :spell="spell" :key="spell.index"></spell-list-item>
     </div>
 </template>
 
 <script>
-
+import SpellListItem from "@/components/spellListItem";
 export default {
     name: "spell-list",
-    props: ["spells"]
+    props: ["spells"],
+    components: {
+        "spell-list-item": SpellListItem
+    }
     
 }
 </script>
---
+++ src/components/spellListItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="spell">
+        <span>{{spell.name}}  Level: {{spell.level}}  School: {{spell.school.index}}  Casting Time: {{spell.casting_time}}</span>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name: "spell-list-item",
+    props: ["spell"]
+
+}
+</script>
+
+<style>
+
+</style>
